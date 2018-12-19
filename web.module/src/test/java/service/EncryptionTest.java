df887271f18fb9b38b89724e845c858c77de9ae6
dmitrii89 <dmitry_cher@mail.ru>
2018-12-19 13:11:56 +0300

    junit test encryption

--- web.module/src/test/java/service/EncryptionTest.java
+++ web.module/src/test/java/service/EncryptionTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotSame;
 
 public class EncryptionTest {
     @Test
